49517cf9c9b50a85bb4900c90fb64b4541231605
Xavier <xdeantabelisario@danielcastelao.org>
2021-03-19 18:29:03 +0100

    proyecto listo

---
+++ Assets/TextWriting2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextWriting2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro GUItext;
+    public float tChar;
+    private string story;
+    int indChar;
+    bool active;
+    float timer;
+    void Start()
+    {
+        story = "Xela Arias Castaño, nació en Lugo en 1962 con el nombre de María de los Ángeles.En 1995 le autorizaron el cambio de nombre por el de Xela.\nEmpezo a trabajar en Edicións Xerais de Galicia, en 1979. Primero realizó trabajos de oficinista, después de creación e investigación y entre 1990 y 1996 como correctora de estilo y editora, además de escritora, poeta y traductora española en varios idiomas.\nEn el curso 1999-2000 comenzó a ejercer como profesora sustituta de enseñanza secundaria, de lengua y literatura castellana, en institutos de Santa Comba, Moaña, Pontevedra, Santiago de Compostela y Vigo. En sus últimos años de vida fue docente en diferentes municipios gallegos, como Chapela, A Guarda y Vigo.\n En noviembre del año 2003 Xela Arias falleció a los 41 años en un hospital de Vigo debido a un ataque cardíaco.";
+
+    }
+
+    void Update()
+    {
+        if(active){
+            timer = timer - Time.deltaTime;
+            if(timer <= 0f){
+                timer = timer + tChar;
+                if(indChar <= story.Length){
+                    GUItext.text = story.Substring(0,indChar);
+                    indChar++;
+                }
+
+            }
+        }
+    }
+
+    public void onFocus(){
+        //Debug.Log("story: " + story);
+        active=true;
+    }
+
+    public void onLost(){
+        //story = GUItext.text;
+        GUItext.text = "";
+        active = false;
+        indChar=1;
+        timer = 0;
+    }
+
+}
---
+++ Assets/TextWriting3.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextWriting3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro GUItext;
+    public float tChar;
+    private string story;
+    int indChar;
+    bool active;
+    float timer;
+    void Start()
+    {
+        story = "O poemario Darío a diario non naceu coa vocación de se lo que hoxe é: un libro encadernado e público. Foi, no seu principio, a necesidade imperiosa de explicarlle ó meu fillo o manifesto íntimo e interno de intencións maternas nas que el mesmo, polo feito de existir, me obrigaba a moverme; o compendio de contradiccións que a maternidade me provoca. Somos mulleres: prestámo-lo noso corpo para que nel se desenvolva un ser intrínseco a nós e ó mesmo tempo independente de nós, alleo a nós; ata aí a función biolóxica da propia muller. Mais despois a criatura chega ó mundo, e velaquí que se dan as diferentes maneiras de vivi-lo que se chama maternidade, a que parte principalmente da obriga social da cría nos primeiros tempos: non podemos esquecelo, a maternidade así é un feito cultural, social, non intrínseco á muller, aínda que si exercido durante séculos e séculos. E pódese vivir seguramente de moitos xeitos: como posesión e arma de muller ou nai; como extensión de ideas ou desexos... Ou, como creo e máis cren comigo, co firme propósito de face-lo posible (e desde logo sen saber que é o posible, primeira contradicción) para axudar ou fomenta-la creación dun ser libre e independente de criterio e corpo, que saiba desenvolverse nesta sociedade ensarillada nunha andamiaxe de valores que non considero nin liberadores nin fomentadores da independencia (segunda gran contradicción). En todo isto, só un enorme territorio sen contradicción posible: o amor eterno e incombustible, sen medo das palabras. Xela Arias.";
+
+    }
+
+    void Update()
+    {
+        if(active){
+            timer = timer - Time.deltaTime;
+            if(timer <= 0f){
+                timer = timer + tChar;
+                if(indChar <= story.Length){
+                    GUItext.text = story.Substring(0,indChar);
+                    indChar++;
+                }
+
+            }
+        }
+    }
+
+    public void onFocus(){
+        //Debug.Log("story: " + story);
+        active=true;
+    }
+
+    public void onLost(){
+        //story = GUItext.text;
+        GUItext.text = "";
+        active = false;
+        indChar=1;
+        timer = 0;
+    }
+
+}
